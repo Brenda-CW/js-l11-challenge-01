f99d05f261808f8439f08d04a815cdf0ba13219d
Brenda-CW <factotum4rays@gmail.com>
2021-09-20 11:27:36 -0600

    pic generator script completed

--- js/script.js
+++ js/script.js
@@ -8,5 +8,29 @@ const getImage = async function () {
         ); 
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images); //runs the selectRandomImages function, part of which involves logging out a random number to make sure the function is working
 };
-getImage();
+
+
+//Randomly Select the INdex of an Image
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random()*images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    console.log(randomImage);
+    displayImage(randomImage);
+};
+
+//Display the Image
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+
+//Add a Click Event 
+button.addEventListener("click", function (e) {
+    getImage();
+})
